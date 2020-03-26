b4fe4a7c66e692acaf7a275d530149197837fc33
John Wiebalk <jwiebalk@github.com>
2020-03-26 11:54:42 -0400

    working

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ require('dotenv').config()
 
 var handler = createHandler({ path: '/webhook', secret: (process.env.SHARED_SECRET) })
 
-var org = process.env.GHE_ORG
+var impersonationToken = ''
+var creator = ''
+var repo_owner = ''
+var repo = ''
 
 
 http.createServer(function (req, res) {
@@ -21,9 +24,20 @@ handler.on('error', function (err) {
 
 handler.on('repository', function (event) {
   if (event.payload.action === 'created') {
-    repo = event.payload.repository.full_name
+    repo_full_name = event.payload.repository.full_name
     creator = event.payload.sender.login
-    console.log('Received webhook for new repository ' + repo + ' created by ' + creator)
+    console.log('Received webhook for new repository ' + repo_full_name + ' created by ' + creator)
+    console.log(repo_full_name.split('/'))
+    repo_name = repo_full_name.split('/')
+    repo_owner = repo_name[0]
+    repo = repo_name[1]
+    if(repo_owner == creator) {
+      console.log("delete the repo")
+      getImpersonation(creator)
+      console.log(impersonationToken)
+    } else {
+      console.log("doing nothing")
+    }
     //addUsertoOrg(user)
   }
 })
@@ -65,5 +79,116 @@ function addUsertoOrg (user) {
   })
 
   req.write(data)
+  req.end()
+}
+
+function getImpersonation (creator) {  const https = require('https')
+  const data = JSON.stringify({
+    scopes: ['delete_repo']
+  })
+
+  const options = {
+    hostname: (process.env.GHE_HOST),
+    port: 443,
+    path: '/api/v3/admin/users/' + creator + '/authorizations',
+    method: 'POST',
+    headers: {
+      Authorization: 'token ' + (process.env.GHE_TOKEN),
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  }
+  let body = []
+  const req = https.request(options, (res) => {
+    res.on('data', (chunk) => {
+      body.push(chunk)
+    }).on('end', () => {
+      body = Buffer.concat(body).toString()
+      if (res.statusCode == 201 || res.statusCode == 200 ) {
+      impersonationToken = JSON.parse(body).token
+      //deleteImpersonationToken()
+      if (impersonationToken === null || impersonationToken === 'null' || impersonationToken.length < 1) {
+        console.log('Creating impersonationToken failed for ' + creator)
+      }
+      deleteRepository()
+    }
+    })
+
+  })
+
+  req.on('error', (error) => {
+    console.error(error)
+  })
+
+  req.write(data)
+  req.end()
+}
+
+function deleteImpersonationToken () {
+  console.log('Deleting impersonation token for ' + creator)
+  const https = require('https')
+
+  const options = {
+    hostname: (process.env.GHE_HOST),
+    port: 443,
+    path: '/api/v3/admin/users/' + creator + '/authorizations',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'token ' + (process.env.GHE_TOKEN),
+      'Content-Type': 'application/json'
+    }
+  }
+  let body = []
+  const req = https.request(options, (res) => {
+    res.on('data', (chunk) => {
+      body.push(chunk)
+    }).on('end', () => {
+      body = Buffer.concat(body).toString()
+      if (res.statusCode == 204) {
+        console.log('impersonationToken for ' + creator + ' deleted')
+      }
+    })
+
+  })
+
+  req.on('error', (error) => {
+    console.error(error)
+  })
+
+  req.end()
+}
+
+function deleteRepository() {
+  console.log('Deleting repo ' + repo_full_name)
+  const https = require('https')
+
+  const options = {
+    hostname: (process.env.GHE_HOST),
+    port: 443,
+    path: '/api/v3/repos/' + repo_full_name,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'token ' + impersonationToken,
+      'Content-Type': 'application/json'
+    }
+  }
+  let body = []
+  const req = https.request(options, (res) => {
+    res.on('data', (chunk) => {
+      body.push(chunk)
+    }).on('end', () => {
+      body = Buffer.concat(body).toString()
+      if (res.statusCode == 204) {
+        console.log('Repository ' + repo_full_name + ' deleted')
+        deleteImpersonationToken()
+      }
+    })
+
+  })
+
+  req.on('error', (error) => {
+    console.error(error)
+  })
+
   req.end()
 }
